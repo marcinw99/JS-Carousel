9476911aaa60fbb654fc49196586ce0bc6f5f1f0
Marcin Wąsik <marcinmw99@gmail.com>
2018-01-12 13:51:30 +0100

    Add buttons to switch slides

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,44 @@
 $(function() {
-		var carouselList = $('#carousel ul');
+	var carouselList = $('#carousel-list');
 
-		setInterval(changeSlides, 3000);
+	var slideChangeOnTime = setInterval(function() { changeSlides(true); }, 5000);
 
-	function changeSlides() {
-		carouselList.animate( { 'marginLeft': -1000 }, 1000, moveFirstSlide );
+	$('#carousel #js-prevBtn').on('click', function() {
+		clearInterval(slideChangeOnTime);
+		changeSlides(false);
+		slideChangeOnTime = setInterval(function() { changeSlides(true); }, 5000);
+	});
+	$('#carousel #js-nextBtn').on('click', function() {
+		clearInterval(slideChangeOnTime);
+		changeSlides(true);
+		slideChangeOnTime = setInterval(function() { changeSlides(true); }, 5000);
+	});
+
+	var carouselSize = $('#carousel-list > li').length;
+	//for (var i = carouselSize; i > 0; i--) {
+		var navMarker = $('<li>');
+		$(navMarker).attr('id', 'marker').text('cosik');
+		$('#carousel-nav').append(navMarker);
+	//}
+
+	function changeSlides(directionNext) {
+		if (directionNext) {
+			carouselList.animate( { 'marginLeft': -1000 }, 1000, function() {moveFirstSlide(true); } );
+		} else {
+			carouselList.animate( { 'marginLeft': 1000 }, 1000, function() {moveFirstSlide(false); } );
+		}
 	}
 
-	function moveFirstSlide() {
+	function moveFirstSlide(NextSlide) {
 		var firstItem = carouselList.find('li:first');
 		var lastItem = carouselList.find('li:last');
-		lastItem.after(firstItem);
-		carouselList.css('margin-left', '0');
+
+		if (NextSlide) {
+			lastItem.after(firstItem);
+			carouselList.css('margin-left', '0');
+		} else {
+			firstItem.before(lastItem);
+			carouselList.css('margin-left', '0');
+		}
 	}
 });
