abf1b8549bb4c4103ec9b0b271730d0502c32612
Marcin Wąsik <marcinmw99@gmail.com>
2018-01-15 18:54:46 +0100

    Working carousel, completely reworked code

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,18 @@
 $(function() {
 	var CONSTANTS = Object.freeze({
-		SLIDE_DELAY: 2000,
+		SLIDE_DELAY: 4000,
 		SLIDE_ANIMATION_TIME: 1000,
-		LEFT_DIRECTION: 'left',
-		RIGHT_DIRECTION: 'right'
+		RIGHT_DIRECTION: 'right',
+		LEFT_DIRECTION: 'left'
 	});
 
-	var carouselList = $('#carousel-list'),
+	var $carouselList = $('#carousel-list'),
 		$carousel = $('#carousel'),
-		$dotsCarousel = $('#carousel-nav');
+		$slideIndicatorsCarousel = $('#carousel-nav');
 
-	var slideChangeOnTime = setInterval(function() { changeSlides(CONSTANTS.RIGHT_DIRECTION); }, CONSTANTS.SLIDE_DELAY);
-
-	$('#js-prevBtn').on('click', function() {		
-		changeSlides(CONSTANTS.LEFT_DIRECTION);
-	});
-
-	$('#js-nextBtn').on('click', function() {
-		changeSlides(CONSTANTS.RIGHT_DIRECTION);
-	});
-
-	function changeSlides(directionNext) {
-		var activeElement = carouselList.find('li.active'),
-			activeIndex = +activeElement.attr('data-id'),
-			nextIndex = activeIndex;
-
-		carouselList.find('li').removeClass('active');
-
-		switch (directionNext) {
-			case CONSTANTS.LEFT_DIRECTION:				
-				if (activeIndex === 0) {
-					nextIndex = carouselList.find('li').length - 1;
-				} else {
-					nextIndex--;
-				}
-
-				moveLastSlide();
-				carouselList.animate( { 'marginLeft': 0 }, CONSTANTS.SLIDE_ANIMATION_TIME);
-				break;
-			case CONSTANTS.RIGHT_DIRECTION:	
-				if ((activeIndex + 1) === carouselList.find('li').length) {
-					nextIndex = 0;
-				} else {
-					nextIndex++;
-				}
-
-				carouselList.animate( { 'marginLeft': -1000 }, CONSTANTS.SLIDE_ANIMATION_TIME, function() {moveFirstSlide(); } );
-				break;
-		}	
-		
-		carouselList.find('li').eq(nextIndex).addClass('active');
-		$dotsCarousel.find('li').removeClass('active').eq(nextIndex).addClass('active');
-	}
-
-	function moveFirstSlide() {
-		var firstItem = carouselList.find('li:first');
-		var lastItem = carouselList.find('li:last');
-
-		lastItem.after(firstItem);
-		carouselList.css('margin-left', '0');	
-	}
-
-	function moveLastSlide() {
-		var firstItem = carouselList.find('li:first');
-		var lastItem = carouselList.find('li:last');
-
-		firstItem.before(lastItem);
-		carouselList.css('margin-left', -1000)
-	}
-
-	function generateDots() {
+	function generateIndicators() {
 		var result = '', 
-			liElementsSize = carouselList.find('li').length;
+			liElementsSize = $carouselList.find('li').length;
 
 		for (var i = 0; i < liElementsSize; i++) {
 			result += '<li></li>';
@@ -80,27 +21,89 @@ $(function() {
 		return $(result);
 	}
 
-	function createDots() {
-		var $dots = generateDots();
-
-		$dotsCarousel.append($dots);
+	function addIndicatorsToDOM() {
+		var $dots = generateIndicators();
+		$slideIndicatorsCarousel.append($dots);
 	}
 
 	function addDataIdToSlides() {
-		carouselList.find('li').each(function(index, item) {
-			$(item).attr('data-id', index)
+		$carouselList.find('li').each(function(index, item) {
+			$(item).attr('data-id', index);
 		});
+	}
+
+	addIndicatorsToDOM();
+	addDataIdToSlides();
+
+	var slideChangeInterval = setInterval(function() { changeSlide(CONSTANTS.RIGHT_DIRECTION); }, CONSTANTS.SLIDE_DELAY);
+
+	function changeSlide(slideDirection) {
+		var activeSlide = $carouselList.find('li.active'),
+			indexOfActiveSlide = activeSlide.attr('data-id'),
+			indexOfNextSlide = indexOfActiveSlide;
+
+		switch (slideDirection) {
+			case CONSTANTS.LEFT_DIRECTION:
+				if (indexOfActiveSlide == 0) {
+					indexOfNextSlide = $carouselList.find('li').length-1;
+				} else {
+					indexOfNextSlide--;
+				}
+
+				moveLastSlide();
+				$carouselList.animate({'marginLeft':0}, CONSTANTS.SLIDE_ANIMATION_TIME, function() { $carouselList.css({marginLeft:0}); });
+				break;
+			case CONSTANTS.RIGHT_DIRECTION:
+				if (indexOfActiveSlide == $carouselList.find('li').length-1) {
+					indexOfNextSlide = 0;
+				} else {
+					indexOfNextSlide++;
+				}
+
+				$carouselList.animate({'marginLeft':-1000}, CONSTANTS.SLIDE_ANIMATION_TIME, moveFirstSlide);
+				break;
+		}
+
+		activeSlide.removeClass('active');
+		var addClassToNextSlide = $carouselList.find('li[data-id='+indexOfNextSlide+']');
+		addClassToNextSlide.addClass('active');
+		$slideIndicatorsCarousel.find('li').removeClass('active').eq(indexOfNextSlide).addClass('active');
+	}
+
+	function moveLastSlide() {
+		var firstItem = $carouselList.find('li:first');
+		var lastItem = $carouselList.find('li:last');
+
+		firstItem.before(lastItem);
+		$carouselList.css({marginLeft:-1000});
+	}
+
+	function moveFirstSlide() {
+		var firstItem = $carouselList.find('li:first');
+		var lastItem = $carouselList.find('li:last');
+
+		lastItem.after(firstItem);
+		$carouselList.css({marginLeft:0});
 	}
 
 	$carousel.hover(
 		function() {
-			clearInterval(slideChangeOnTime);
-		},
+			clearInterval(slideChangeInterval);
+		}, 
 		function() {
-			slideChangeOnTime = setInterval(function() { changeSlides(CONSTANTS.RIGHT_DIRECTION); }, CONSTANTS.SLIDE_DELAY);
+			slideChangeInterval = setInterval(function() { changeSlide(CONSTANTS.RIGHT_DIRECTION); }, CONSTANTS.SLIDE_DELAY);
 		}
 	);
 
-	createDots();
-	addDataIdToSlides();
+	$('#js-prevBtn').on('click', function() {	
+		$('#js-prevBtn').prop('disabled', true);	
+		changeSlide(CONSTANTS.LEFT_DIRECTION);
+		setTimeout(function() { $('#js-prevBtn').prop('disabled', false); }, CONSTANTS.SLIDE_ANIMATION_TIME);
+	});
+
+	$('#js-nextBtn').on('click', function() {
+		$('#js-nextBtn').prop('disabled', true);	
+		changeSlide(CONSTANTS.RIGHT_DIRECTION);
+		setTimeout(function() { $('#js-nextBtn').prop('disabled', false); }, CONSTANTS.SLIDE_ANIMATION_TIME);
+	});
 });
